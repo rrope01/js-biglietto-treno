71da7aefe5a8bbe63129cc1b221240cce38699d4
Matteo Perrone <teo.perro01@gmail.com>
2026-05-09 16:20:00 +0200

    all

---
+++ script.js
@@ -0,0 +1,12 @@
+const pricePerKm = 0.21;
+const distance = parseFloat(prompt("Quanti km devi percorrere?"));
+const age = parseInt(prompt("Quanti anni hai?"));
+var finalPrice = 0;
+if (age < 18) {
+    finalPrice = (pricePerKm * distance) * 0.8;
+} else if (age > 65) {
+    finalPrice = (pricePerKm * distance) * 0.6;
+} else {
+    finalPrice = pricePerKm * distance;
+}
+console.log(`Il prezzo del biglietto è: ${finalPrice.toFixed(2)}€`);
